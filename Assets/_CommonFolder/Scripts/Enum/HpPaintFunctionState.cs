6ed77ba8a9d74c6888ae741e6f92a9943e157d11
KENTO <kentou1506@gmail.com>
2020-03-20 17:12:42 +0900

    Modify Redo Undo

---
+++ Assets/_CommonFolder/Scripts/Enum/HpPaintFunctionState.cs
@@ -0,0 +1,28 @@
+﻿namespace Hp
+{
+    /// <summary>
+    /// お絵描きモードのステート
+    /// </summary>
+    public enum HpPaintFunctionState
+    {
+        /// <summary>
+        /// 何の機能も使ってない
+        /// </summary>
+        NoFunc,
+
+        /// <summary>
+        /// 元に戻すモード
+        /// </summary>
+        Redo,
+
+        /// <summary>
+        /// 元に戻したものを元に戻すモード
+        /// </summary>
+        Undo,
+
+        /// <summary>
+        /// お絵描きモード
+        /// </summary>
+        Paint
+    }
+}
